6acd760b7b42561900a67a2e4d59698c1f3e3e24
Baloohoney <baloohoney@gmail.com>
2023-03-28 21:50:41 +0300

    Function edited

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,51 @@
 fun main() {
-    println(
-        bankFeeCalculation(
-            paymentSystem = "MasterCard",
-            monthTotal = 75000.0,
-            transactionAmount = 5000.0
+    if (bankFeeCalculation(
+            paymentSystem = "VK Pay",
+            monthTotal = 30_000,
+            transactionAmount = 1_000
+        ) == -1) println("Перевод невозможен")
+    else println(bankFeeCalculation(
+            paymentSystem = "VK Pay",
+            monthTotal = 30_000,
+            transactionAmount = 1_000
         )
     )
 }
 
 fun bankFeeCalculation(
-    paymentSystem: String = "VK",
-    monthTotal: Double = 0.0,
-    transactionAmount: Double
-) = when (paymentSystem) {
-    "MasterCard" -> if (monthTotal < 75000.0) 0.0
-    else (transactionAmount * 0.006) + 20.0
+    paymentSystem: String,
+    monthTotal: Int,
+    transactionAmount: Int
+): Int {
+    return when (paymentSystem) {
+        "MasterCard" -> when {
+            transactionAmount > 150_000 || transactionAmount + monthTotal > 600_000 -> -1
+            monthTotal <= 75000.0 -> 0
+            else -> (transactionAmount * 0.006).toInt() + 20
+        }
 
-    "Visa_Mir" -> transactionAmount * 0.0075
-    else -> 0.0
+        "Maestro" -> when {
+            transactionAmount > 150_000 || transactionAmount + monthTotal > 600_000 -> -1
+            monthTotal <= 75000.0 -> 0
+            else -> (transactionAmount * 0.006).toInt() + 20
+        }
+
+        "Visa" -> when {
+            transactionAmount > 150_000 || transactionAmount + monthTotal > 600_000 -> -1
+            else -> (transactionAmount * 0.0075).toInt()
+        }
+
+        "Mir" -> when {
+            transactionAmount > 150_000 || transactionAmount + monthTotal > 600_000 -> -1
+            else -> (transactionAmount * 0.0075).toInt()
+        }
+
+        "VK Pay" -> when {
+            transactionAmount > 15_000 || transactionAmount + monthTotal > 40_000 -> -1
+            else -> 0
+        }
+
+        else -> 0
+
+    }
 }
